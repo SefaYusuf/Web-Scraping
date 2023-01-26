8f5bf115465bfdd29ee6221a1ce8424a029de4b8
Yusuf Sozkeser <yusufsefa1993@gmail.com>
2023-01-26 12:57:09 +0100

    Web Scraping L2

--- _requests.py
+++ _requests.py
@@ -1,31 +1,16 @@
 import requests
 import json
-class Product:
-    def __init__(self) -> None:
-        self.url = "https://dummyjson.com/products"
-    def get_all_product(self):
-        respose = requests.get(self.url)
-        # products = json.loads(respose.text) # ilk kullanım
-        products = respose.json() #ikinci kullanım
-    def get_single_product(self, product_id):
-        response =  requests.get(f"{self.url}/{product_id}")
-        #https://dummyjson.com/products/product_id
-        product = response.json()
-        return product
-        return products
-    #https://dummyjson.com/products/search?q=phone
-    def search_product(self, name):
-        pyload = {'q': name}
-        response = requests.get(f"{self.url}/search/", params=pyload)
-        product = response.json()
-        return product
-      #https://dummyjson.com/products?limit=10&skip=10&select=title,price
-    def select_product(self, limit, skip, select):
-        pyload = {'limit': limit, 'skip': skip , 'select': select}
-        response = requests.get(f"{self.url}", params=pyload)
-        return response.json()
-product = Product()
-# print(product.get_all_product())
-# print(product.get_single_product(5))
-print(product.search_product('phone'))
-print(product.select_product(10, 10, 'title'))
+# print(products['products'])
+# user = "{'username': 'ömer', 'age': 27, 'email': 'omer@gmail.com'}"
+# print(user['username'])
+url = "https://dummyjson.com/products"
+response = requests.get(url, params={'limit': 15, 'skip': 20})
+print(response.status_code)
+products = response.text
+print(type(products))
+products = json.loads(response.text)
+print(type(products))
+# products = products['products']
+# for product in products:
+#     print(product['title'])
+print(products)
